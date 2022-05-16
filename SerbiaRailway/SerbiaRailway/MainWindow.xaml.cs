bb174c2551bb4359b688100ab5a13a6043f140ef
Anđela <andjelamiskovic2000@gmail.com>
2022-05-16 17:29:20 +0200

    added login page

--- SerbiaRailway/SerbiaRailway/MainWindow.xaml.cs
+++ SerbiaRailway/SerbiaRailway/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace SerbiaRailway
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
