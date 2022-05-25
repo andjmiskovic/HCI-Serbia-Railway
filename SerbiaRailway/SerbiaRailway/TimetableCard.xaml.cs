63a28d7ac07034ed836f13fbabb06d2bd0416af7
Anđela <andjelamiskovic2000@gmail.com>
2022-05-25 22:15:36 +0200

    added ui for timetable

--- SerbiaRailway/SerbiaRailway/TimetableCard.xaml.cs
+++ SerbiaRailway/SerbiaRailway/TimetableCard.xaml.cs
@@ -16,14 +16,13 @@ using System.Windows.Shapes;
 namespace SerbiaRailway
 {
     /// <summary>
-    /// Interaction logic for TimetableClient.xaml
+    /// Interaction logic for TicketCard.xaml
     /// </summary>
-    public partial class TimetableClient : Page
+    public partial class TimetableCard : UserControl
     {
-        public TimetableClient()
+        public TimetableCard()
         {
             InitializeComponent();
-            // FromSelect.ItemsSource = 
         }
     }
 }
