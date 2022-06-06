6763688b78b6c10d311ec01756268f9e0955b0b2
Anđela <andjelamiskovic2000@gmail.com>
2022-06-06 15:30:07 +0200

    added manager tickets search

---
+++ SerbiaRailway/SerbiaRailway/TicketCardManager.xaml.cs
@@ -0,0 +1,33 @@
+﻿using SerbiaRailway.model;
+using System;
+using System.Windows.Controls;
+
+namespace SerbiaRailway
+{
+    /// <summary>
+    /// Interaction logic for TicketCard.xaml
+    /// </summary>
+    public partial class TicketCardManager : UserControl
+    {
+        public Ticket ticket { get; set; }
+        public TicketCardManager(Ticket ticket)
+        {
+            InitializeComponent();
+            this.ticket = ticket;
+            FillData();
+        }
+
+        private void FillData()
+        {
+            StartTime.Content = ticket.PartialLine.StartTime.ToString().Substring(0, 5);
+            EndTime.Content = ticket.PartialLine.EndTime.ToString().Substring(0, 5);
+            StartStation.Content = ticket.PartialLine.Start.Name;
+            EndStation.Content = ticket.PartialLine.End.Name;
+            TimeSpan duration = DateTime.Parse(ticket.PartialLine.EndTime.ToString()).Subtract(DateTime.Parse(ticket.PartialLine.StartTime.ToString()));
+            WagonNumber.Content = "Wagon: " + ticket.Wagon + ", Seat: " + ticket.Seat.SeatNumber;
+            Price.Content = "Price: " + ticket.Price.ToString() + "din";
+            Date.Content = ticket.Date.ToString("MM/dd/yyyy");
+            Client.Content = ticket.Client.Name + " " + ticket.Client.LastName;
+        }
+    }
+}
--- SerbiaRailway/SerbiaRailway/TicketsReport.xaml.cs
+++ SerbiaRailway/SerbiaRailway/TicketsReport.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using SerbiaRailway.model;
+using SerbiaRailway.services;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace SerbiaRailway
 {
@@ -10,6 +14,82 @@ namespace SerbiaRailway
         public TicketsReport()
         {
             InitializeComponent();
+            FillLines();
+        }
+
+        private void FillMonthlyTickets(int month, int year)
+        {
+            List<Ticket> tickets;
+            foreach (Ticket ticket in DataService.Data.GetTickets())
+            {
+                if(correctMonth(ticket.Date, month, year))
+                    Tickets.Children.Add(new TicketCardManager(ticket));
+            }
+        }
+
+        private bool checkRide(Ticket ticket, int lineId, DateTime date)
+        {
+            return ticket.PartialLine.Line.Id == lineId && date == ticket.Date;
+        }
+
+        private void FillRideTickets(int lineId, DateTime date)
+        {
+            List<Ticket> tickets;
+            foreach (Ticket ticket in DataService.Data.GetTickets())
+            {
+                if (checkRide(ticket, lineId, date))
+                    Tickets.Children.Add(new TicketCardManager(ticket));
+            }
+        }
+
+        private bool correctMonth(DateTime date, int month, int year)
+        {
+            return date.Year == year && date.Month == month;
+        }
+
+        private void FillLines()
+        {
+            Line.Items.Clear();
+            foreach(Line line in DataService.Data.GetLines()) 
+            {
+                Line.Items.Add(line.Id + " (" + line.Name + ")");
+            }
+        }
+
+        private void Monthly_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (Year != null)
+            {
+                Year.Visibility = System.Windows.Visibility.Visible;
+                Month.Visibility = System.Windows.Visibility.Visible;
+                Line.Visibility = System.Windows.Visibility.Hidden;
+                Date.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Ride_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (Year != null)
+            {
+                Year.Visibility = System.Windows.Visibility.Hidden;
+                Month.Visibility = System.Windows.Visibility.Hidden;
+                Line.Visibility = System.Windows.Visibility.Visible;
+                Date.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DateTime date = Date.SelectedDate.Value;
+            int year = int.Parse(Year.SelectedItem.ToString());
+            int month = Month.SelectedIndex + 1;
+            int lineId = int.Parse(Line.SelectedItem.ToString().Split('(')[0]);
+
+            Tickets.Children.RemoveRange(0, Tickets.Children.Count);
+            if ((bool)MonthlyTickets.IsChecked)
+                FillMonthlyTickets(month, year);
+            else
+                FillRideTickets(lineId, date);
         }
     }
 }
